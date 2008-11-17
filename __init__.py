5ce6c25081bf40022d4ad513318d09696a4ce176
Naitik Shah <naitik.shah@yahoo.com>
2008-11-16 16:06:28 -0800

    initial commit

---
+++ __init__.py
@@ -0,0 +1,27 @@
+class cached_property(object):
+    """
+    A decorator that converts a function into a lazy property. The
+    function wrapped is called the first time to retrieve the result
+    and than that calculated result is used the next time you access
+    the value::
+
+        class Foo(object):
+
+            @cached_property
+            def foo(self):
+                # calculate something important here
+                return 42
+
+    """
+
+    def __init__(self, func, name=None, doc=None):
+        self.func = func
+        self.__name__ = name or func.__name__
+        self.__doc__ = doc or func.__doc__
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        value = self.func(obj)
+        setattr(obj, self.__name__, value)
+        return value
